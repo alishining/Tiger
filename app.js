a190cdecd124282a20d6ad3a49029bf869397e6c
alishining <shine1988@126.com>
2015-12-03 17:19:30 +0800

    null

--- app.js
+++ app.js
@@ -85,6 +85,8 @@ app.use(function(err, req, res, next) {
 	});
 });
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app);
+server.timeout = 0;
+server.listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
 });
