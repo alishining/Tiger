041fc8c2f27ce16c91148770a23a1d7f3ec508d3
alishining <shine1988@126.com>
2016-01-25 13:27:03 +0800

    update

--- app.js
+++ app.js
@@ -30,6 +30,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.all('*', function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+	res.header("X-Powered-By",' 3.2.1')
+	res.header("Content-Type", "application/json;charset=utf-8");
+	next();
+});
+
 app.get('/create_subject', routes.create_subject);
 app.post('/post_subject_info', routes.post_subject_info);
 app.post('/create_student', routes.create_student);
